47dc1e5a6e466be7817c8da0b30ea393b5b6b718
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-04 19:51:49 +0530

    added routes , model and middleware auth

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,17 +1,40 @@
-const express = require('express');
-const { default: mongoose } = require('mongoose');
-const mongodb = require('mongoose');
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 
-const mongoUri =
-  'mongodb+srv://track-on-map:9y9DbYeD7oglLif8@cluster0.pwqksnv.mongodb.net/test';
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
 
-  mongoose.connect(mongoUri,)
-app.get('/', (req, res) => {
-  res.send('hi there !');
+const mongoUri = 'mongodb+srv://track-on-map:9y9DbYeD7oglLif8@cluster0.pwqksnv.mongodb.net/test';
+if (!mongoUri) {
+  throw new Error(
+    `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
+  );
+}
+
+mongoose.set("strictQuery", true);
+// resolves future deprecation issue with Mongoose v7
+
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3001, () => {
-  console.log('listening to port 3001');
+  console.log("Listening on port 3001");
 });
