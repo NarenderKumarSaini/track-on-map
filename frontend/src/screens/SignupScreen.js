0ce19ce0043afab441fe243143e56f43da17b425
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-07 12:07:37 +0530

    resturucted the project

---
+++ frontend/src/screens/SignupScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { StyleSheet } from 'react-native';
+import {
+  Button,
+  Input,
+  Text,
+} from 'react-native-elements';
+
+import Spacer from '../components/Spacer';
+
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
