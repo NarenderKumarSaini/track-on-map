5a145822573e64731ec193720ceead176600ae7f
Narender Kumar Saini <narender.itrustss@gmail.com>
2023-02-09 13:46:34 +0530

    Added safearea to fix top bar overlap issue

--- frontend/src/screens/AccountScreen.js
+++ frontend/src/screens/AccountScreen.js
@@ -5,6 +5,7 @@ import {
   Text,
 } from 'react-native';
 import { Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
 
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
@@ -13,12 +14,12 @@ const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
 
   return (
-    <>
+    <SafeAreaView forceInset={{ top: 'always' }}>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signout} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
